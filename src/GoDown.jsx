d025c446d5714e3b7fedefb538467d7170b15906
Santhosh-Charanthu <santhoshcharanthu455@gmail.com>
2025-03-23 14:04:38 +0530

    removed hover

--- src/GoDown.jsx
+++ src/GoDown.jsx
@@ -20,9 +20,6 @@ export default function GoDown() {
           <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"></path>
         </svg>
       </button>
-      <span className="hover-text">
-        <b>Go Down</b>
-      </span>
 
       {/* Fullscreen Scrollable Overlay */}
       <div className={`overlay-page ${isOpen ? "active" : ""}`}>
